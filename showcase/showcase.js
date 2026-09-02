b062e5639bf98cba0f360c08db0b21f55ba17e67
sanazarifargroup <sa.nazarifar@gmail.com>
2026-09-02 19:12:56 +0330

    Hide enquiry prompt on all image showcase pages

--- showcase/showcase.js
+++ showcase/showcase.js
@@ -378,10 +378,3 @@ window.addEventListener("resize", () => { centerActive(); fitBrandName(); });
 if (document.fonts?.ready) document.fonts.ready.then(fitBrandName);
 applyLanguage();
 if (projectCount) selectProject(0);
-
-if (!document.querySelector('script[data-nf-inquiry-loader]')) {
-  const inquiryScript = document.createElement("script");
-  inquiryScript.src = "/inquiry/inquiry.js?v=17";
-  inquiryScript.dataset.nfInquiryLoader = "true";
-  document.body.append(inquiryScript);
-}
